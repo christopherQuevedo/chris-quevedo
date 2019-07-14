f06cfa06cc115b3ea4348d20ea4db42dad249da1
Chris Quevedo <chris.quevedo@yahoo.com>
2019-07-13 21:58:57 -0700

    created navbar and background

---
+++ index.js
@@ -0,0 +1,17 @@
+function openSideMenu(){
+    let sideMenu = document.getElementById("side-menu");
+    sideMenu.style.width = '250px';
+    /*
+    let mainContent = document.getElementById("main");
+    mainContent.style.marginLeft = '250px';
+    */
+}
+
+function closeSideMenu(){
+    let sideMenu = document.getElementById("side-menu");
+    sideMenu.style.width = '0';
+    /*
+    let mainContent = document.getElementById("main");
+    mainContent.style.margin = 'auto';
+    */
+}
