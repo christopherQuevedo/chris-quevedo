80cd1fa8de7db189456511d75f468c980cb5face
Chris Quevedo <chris.quevedo@yahoo.com>
2019-07-14 22:19:15 -0700

    added home, projects, resume

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
+document.getElementById("home").onclick = clickHome;
+document.getElementById("projects").onclick = clickProjects;
+document.getElementById("resume").onclick = clickResume;
+
+document.getElementById("homeScreen").style.display = "";
+document.getElementById("projectsScreen").style.display = "none";
+document.getElementById("resumeScreen").style.display = "none";
+
+
+/**
+ * Function to open side nav bar when clicking on the hamburger
+ */
 function openSideMenu(){
     let sideMenu = document.getElementById("side-menu");
     sideMenu.style.width = '250px';
-    /*
-    let mainContent = document.getElementById("main");
-    mainContent.style.marginLeft = '250px';
-    */
 }
 
+/**
+ * Function to close side nav bar when clicking on the 'X'
+ */
 function closeSideMenu(){
     let sideMenu = document.getElementById("side-menu");
     sideMenu.style.width = '0';
-    /*
-    let mainContent = document.getElementById("main");
-    mainContent.style.margin = 'auto';
-    */
 }
+
+function clickHome(){
+    closeSideMenu();
+    document.getElementById("homeScreen").style.display = "";
+    document.getElementById("projectsScreen").style.display = "none";
+    document.getElementById("resumeScreen").style.display = "none";
+}
+
+function clickProjects(){
+    closeSideMenu();
+    document.getElementById("homeScreen").style.display = "none";
+    document.getElementById("projectsScreen").style.display = "";
+    document.getElementById("resumeScreen").style.display = "none";
+}
+
+function clickResume(){
+    closeSideMenu();
+    document.getElementById("homeScreen").style.display = "none";
+    document.getElementById("projectsScreen").style.display = "none";
+    document.getElementById("resumeScreen").style.display = "";
+    console.log("bruh");
+}
+
+
